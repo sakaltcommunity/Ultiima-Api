fee69491fa0ffbb4ed8bc496d00cb67bf4512ec9
Ingushe <167761978+Sakalt@users.noreply.github.com>
2025-01-30 18:02:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 app = FastAPI()
 
-MODEL_NAME = "Sakalti/Lunar-4B"
+MODEL_NAME = "Sakalti/ultiima-78B"
 model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
 @app.post("/api-sakal-models")
-async def generate_text(prompt: str, max_new_tokens: int = 50, temperature: float = 1.0, top_p: float = 1.0, top_k: int = 50):
+async def generate_text(prompt: str, max_new_tokens: int = 100, temperature: float = 0.7, top_p: float = 1.0, top_k: int = 50):
     input_ids = tokenizer.encode(prompt, return_tensors="pt")
     output = model.generate(
         input_ids,
